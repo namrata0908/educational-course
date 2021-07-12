9b81b76a4794abc94ac24614b64586de780f56c0
sans_01 <sanchayofficial@gmail.com>
2021-07-12 09:16:37 +0530

    ui changes

--- assets/js/Scroll-Effect.js
+++ assets/js/Scroll-Effect.js
@@ -6,8 +6,39 @@ $(window).scroll(function() {
         $('#return-to-top').fadeOut(200);   // Else fade out the arrow
     }
 });
+
+
 $('#return-to-top').click(function() {      // When arrow is clicked
     $('body,html').animate({
         scrollTop : 0                       // Scroll to top of body
     }, 500);
+});
+
+$('.navbar a').on('click',function(e){
+    if(this.hash !== '') {
+        e.preventDefault();
+
+        const hash = this.hash;
+
+        $('html,body').animate(
+            {
+                scrollTop: $(hash).offset().top     
+            }, 
+            700
+        );
+    }
+});
+
+$('.links a').on('click',function(e){
+    if(this.hash !== '') {
+        e.preventDefault();
+        const hash = this.hash;
+
+        $('html,body').animate(
+            {
+                scrollTop: $(hash).offset().top     
+            }, 
+            500
+        );
+    }
 });
