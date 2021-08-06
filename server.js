e329633a083a6da7ce3113e0073b0c7f8962e82e
amogh0503 <amoghagarwal0503@gmail.com>
2021-08-07 00:16:59 +0530

    IP change

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const port = process.env.PORT || 5000
 const mongoose = require('mongoose')
-const url = "mongodb://localhost/studentData"
+const url = "mongodb://165.22.216.207/studentData"
 
 const app = express();
 mongoose.connect(url, {useNewUrlParser:true})
