b5c39f8bc952d00b2cbd5b11cd3c398c6c1d48f9
sans_01 <sanchayofficial@gmail.com>
2021-09-14 13:49:54 +0530

    added back

--- server.js
+++ server.js
@@ -87,7 +87,9 @@ app.get("/login",function (req,res) {
 		.sendFile(path.join(__dirname,"signin.html"));
 });
 
-
+app.get("/bootcamp",function(req,res) {
+	res.redirect('https://form.typeform.com/to/LUGW5Tqx');
+});
 
 app.get("/*",function (req,res) {
 	res
