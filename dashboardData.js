e7eda12c20c69daee969d286b471e2e508580beb
Manoj sengar <msengar175@gmail.com>
2022-04-01 00:12:42 +0530

    profile pic change

--- dashboardData.js
+++ dashboardData.js
@@ -1,7 +1,7 @@
 function dashboardData(){
     return ({"payment":6000,
-    "sessionDone":1,
-    "completion":50
+    "sessionDone":5,
+    "completion":60
     })
 }
 
@@ -10,4 +10,16 @@ async function showPayment(){
     const paymentDisplay = document.querySelector('#payment')
         
     paymentDisplay.outerHTML="<span>"+"Rs. "+dashboardData().payment+"</span>"
+}
+async function showSessions(){
+
+    const sessionsDisplay = document.querySelector('#sessions')
+        
+    sessionsDisplay.outerHTML="<span>"+dashboardData().sessionDone+"</span>"
+}
+async function showCompletion(){
+
+    const completionDisplay = document.querySelector('#completion')
+        
+    completionDisplay.outerHTML="<span>"+dashboardData().completion +"%"+"</span>"
 }
---
+++ profileData.js
@@ -0,0 +1,19 @@
+function profileData(){
+    return ({"firstname":"Namrata",
+    "lastname":"Sachin"
+    })
+}
+
+// async function showImage(){
+
+//     const completionDisplay = document.querySelector('#dp')
+//     const first = profileData().firstname.charAt(0)
+//     const last = profileData().lastname.charAt(0)
+//     completionDisplay.outerHTML = "<img id="+"profileImage"+"></img>"
+//     document.querySelector('#profileImage').innerText = first+last
+
+//     // completionDisplay.outerHTML="<span>"+dashboardData().completion +"%"+"</span>"
+// }
+
+
+/* <img class="rounded-circle mb-3 mt-4" src="assets/img/hv_new.png" width="160" height="160"></img> */
